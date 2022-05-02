aaa732e0c127d6bae48f02e895f92c7801e0899f
Quentin-NA <quentin.natoly@gmail.com>
2022-05-02 18:28:16 +0200

    updating inbox projet to solidity 0.8.9

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'famous weird identify hint tiny grace photo bonus black cheese warrior faint',
+    'https://rinkeby.infura.io/v3/0311aec30e154a1fa63c030901b3a08d'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
